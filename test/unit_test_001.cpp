ccd6c68f14a33d05192cc24fc15588dae4733ccf
Rob Tillaart <rob.tillaart@gmail.com>
2023-05-18 19:56:24 +0200

    fix

--- test/unit_test_001.cpp
+++ test/unit_test_001.cpp
@@ -86,6 +86,13 @@ unittest(test_get)
     int idx = tm.count();
     assertEqualFloat(i, tm.get(idx), 0.001);
   }
+
+  for (int i = 0; i < tm.count(); i++)
+  {
+    fprintf(stderr, "%f\t", tm.get(idx));
+  }
+
+  fprintf(stderr, "\n");
 }
 
 
